b9abbaa8d817af25bfb928bf6f93dd179a5a5a7e
Arsen <arsproger@gmail.com>
2023-02-11 20:16:13 +0600

    add flyway

---
+++ src/main/resources/db/migration/V1__Init_DB.sql
@@ -0,0 +1,21 @@
+create sequence task_seq start 1 increment 1;
+create sequence users_seq start 1 increment 1;
+
+create table task (
+    id int8 not null,
+    description varchar(255),
+    issued_date timestamp,
+    status varchar(255),
+    title varchar(50) not null,
+    user_id int8,
+    primary key (id));
+
+create table users (
+    id int8 not null,
+    email varchar(30) not null,
+    name varchar(30) not null,
+    password varchar(255) not null,
+    primary key (id) );
+
+alter table if exists users add constraint email_constraint unique (email);
+alter table if exists task add constraint users_constraint foreign key (user_id) references users;
