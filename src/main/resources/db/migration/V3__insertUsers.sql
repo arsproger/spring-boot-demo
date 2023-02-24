d9ed4cc847bf2fab15ca948ba39744ee4bec329e
erlan_alisherov <alisherovv7@bk.ru>
2023-02-24 18:55:34 +0600

    first commit

---
+++ src/main/resources/db/migration/V3__insertUsers.sql
@@ -0,0 +1,3 @@
+insert into users(id,name,email,password,role)
+values (1,'Sasha','sasha@gmail','123qwe','admin'),
+       (2,'Misha','misha@gmail','qwe123','user');
---
+++ src/main/resources/db/migration/V4__insertTask.sql
@@ -0,0 +1,5 @@
+insert into task(id,description,issued_date,status,title,user_id)
+values (1,'N','2023-02-21 20:27:00.000000','NEW','News1',1),
+       (2,'E','2023-02-22 20:27:00.000000','NEW','News2',2),
+       (3,'W','2023-02-23 20:27:00.000000','NEW','News3',2),
+       (4,'S','2023-02-20 20:27:00.000000','NEW','News4',1);
